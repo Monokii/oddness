628c345f0e2e8892e3891618dcb8a160e375618f
Oddness <71020988+Oddnessz@users.noreply.github.com>
2024-06-16 21:54:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
     function animateBlock() {
         block.style.opacity = '1';
         block.style.transform = 'scale(1)';
-        block.style.transition = 'transform 7s cubic-bezier(0.16, 0.69, 0.52, 0.83), opacity 7s ease-in-out';
+        block.style.transition = 'none'; // 이전 트랜지션 효과를 초기화
 
         setTimeout(() => {
-            block.style.transform = 'scale(15)';
-            block.style.opacity = '0';
+            block.style.transition = 'transform 7s cubic-bezier(0.16, 0.69, 0.52, 0.83), opacity 7s ease-in-out';
+            block.classList.add('boom');
         }, 100);
+
+        setTimeout(() => {
+            block.classList.remove('boom');
+        }, 7100);
 
         setTimeout(animateBlock, 14000); // 반복을 위해 14초 후에 다시 실행
     }
